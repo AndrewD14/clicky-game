af91b2de23c7b6bd1cd26c041a89a59917127bea
ajd2144 <andrew.damico@northwestern.edu>
2018-01-31 16:32:40 -0600

    Added in the main app component.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Board from './components/Board/Board';
+
+const App = () => (
+    <div class="container-fluid">
+      <Board />
+    </div>
+)
+
+export default App;
